07637d4f63e5673526afd72093a35f778885ab85
SyahrezaAdnanAlAzhar <syahreza.adnan29@gmail.com>
2026-02-10 11:40:56 +0700

    implement Turnstile

--- src/constants/emailjs.ts
+++ src/constants/emailjs.ts
@@ -1,3 +1,4 @@
 export const EMAILJS_PUBLIC_KEY = 'XzVY4s3rvlJAtpfKE';
 export const EMAILJS_SERVICE_ID = 'service_uzn7rgp';
 export const EMAILJS_TEMPLATE_ID = 'template_ph2en0p';
+export const TURNSTILE_SITE_KEY = '0x4AAAAAACZ7HKZW8Q1QuKCq';
